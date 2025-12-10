ef62f4879ac7ce60028d789076bc4b383e7bb90f
Syed Maaz <syedmaaz78622@gmail.com>
2025-12-10 08:51:07 +0530

    Python basics

--- python_Learning/part4.py
+++ python_Learning/part4.py
@@ -59,3 +59,31 @@ else:
         print("Invalid username")
     else:
         print("Invalid password")
+
+# --------------------------------------------------
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Choose operation: +  -  *  /")
+op = input("Enter operator: ")
+
+match op:
+
+    case "+":
+        print("Result =", num1 + num2)
+
+    case "-":
+        print("Result =", num1 - num2)
+
+    case "*":
+        print("Result =", num1 * num2)
+
+    case "/":
+        if num2 == 0:
+            print("Cannot divide by zero!")
+        else:
+            print("Result =", num1 / num2)
+
+    case _:
+        print("Invalid operator!")
