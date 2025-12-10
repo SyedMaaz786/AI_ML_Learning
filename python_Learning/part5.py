2bb64b8a1e18c737d2e4e86ee613dc71b783b1f7
Syed Maaz <syedmaaz78622@gmail.com>
2025-12-10 09:33:06 +0530

    Python basics

--- python_Learning/part5.py
+++ python_Learning/part5.py
@@ -65,9 +65,20 @@ while(i <=10):
 i = 1
 
 while i <= 10:
+    i += 1
     if i == 6:
         break
     print(i)
-    i += 1
+    
 
+# --------------------------------
+
+i = 0
+
+while i <= 10:
+    i += 1
+    if i == 6:
+        continue
+    print(i)
+    
 
