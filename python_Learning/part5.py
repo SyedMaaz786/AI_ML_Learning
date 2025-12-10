733ac3e19c0b5ee062cdc026ad537015b6ceac48
Syed Maaz <syedmaaz78622@gmail.com>
2025-12-10 09:11:32 +0530

    Python basics

--- python_Learning/part5.py
+++ python_Learning/part5.py
@@ -14,3 +14,47 @@ for ch in "Maaz":
 for i in range(2, 11, 2):
     print(i)
 
+# --------------------------------
+
+for i in range(1 , 11):
+    if i % 2 == 0:
+        print(f"{i} is even")
+    else:
+        print(f"{i} is odd") 
+
+# WHILE LOOP Examples
+
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1     
+
+
+# --------------------------------
+
+i = 5
+
+while i >= 1:
+    print(i)
+    i -= 1
+
+
+# --------------------------------
+
+word = ""
+
+while word != "stop":
+    word = input("Type something (type 'stop') to end")
+
+
+# --------------------------------
+
+count = 1  #iteration variable
+while (count <= 5):
+    print("Hello, Maaz!", count)
+    count += 1
+print("After the loop, count =", count)
+
+# --------------------------------
+
