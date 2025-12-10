b71108531653ee90702d251da16224689eb54c69
Syed Maaz <syedmaaz78622@gmail.com>
2025-12-10 08:58:08 +0530

    Python basics

---
+++ python_Learning/part5.py
@@ -0,0 +1,16 @@
+# FOR LOOP Examples
+
+for i in range(1, 6):
+    print(i)
+
+# --------------------------------
+
+
+for ch in "Maaz":
+    print(ch)
+
+# --------------------------------
+
+for i in range(2, 11, 2):
+    print(i)
+
