940e538f8e1b5ff069d7653fd9b6ca2789102469
Syed Maaz <syedmaaz78622@gmail.com>
2025-12-10 12:28:40 +0530

    Python basics

---
+++ python_Learning/part8.py
@@ -0,0 +1,19 @@
+# Classes and objects in Python
+
+class Student:
+    pass   # empty class
+
+s1 = Student() # creating object
+print(s1)
+
+#--------------------------------
+
+class Car:
+
+    brand = "Toyota"  #properties
+    color = "Red"
+
+c1 = Car()      #creating object
+print(c1.brand)
+print(c1.color)
+
