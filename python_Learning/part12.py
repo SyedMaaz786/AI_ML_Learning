08e99fd0d114ebe41ce3b3ecf70e74363011156a
Syed Maaz <syedmaaz78622@gmail.com>
2025-12-11 12:03:29 +0530

    Python Core

--- python_Learning/part12.py
+++ python_Learning/part12.py
@@ -26,3 +26,35 @@ print(type(json_str), "\n", json_str, "\n")
 py_loaded = json.loads(json_str)            # Convert JSON string back to dictionary
 print(type(py_loaded), "\n", py_loaded, "\n")
 
+
+#---------------------------------------------------------------------------------------------
+# Writing JSON to a file and reading it back(Using file handling)
+import json
+
+data = {
+    "student": {
+        "name": "Maaz",
+        "age": 22,
+        "city": "Hyderabad"
+    },
+    "numbers": [10, 20, 30, 40, 50],
+    "employees": [
+        {"id": 1, "name": "Maaz", "role": "Developer"},
+        {"id": 2, "name": "Irfan", "role": "Designer"},
+        {"id": 3, "name": "Rizwan", "role": "Tester"}
+    ]
+}
+
+# Writing JSON to file using dump()
+with open("data.json", "w") as f:
+    json.dump(data, f, indent=4)
+
+
+# Read JSON back using load()
+with open("data.json", "r") as f:
+    loaded_data = json.load(f)
+
+print(loaded_data)
+print(type(loaded_data))   
+
+
